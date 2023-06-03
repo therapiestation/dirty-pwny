bd79bd9daef201b1eccc655a1379eac43207c01b
therapiestation <oliver.pluess@gmail.com>
2023-06-03 08:27:19 +0200

    changes...

--- dnspony.py
+++ dnspony.py
@@ -28,6 +28,26 @@ def dns_query_specific_nameserver(query="inlanefreight.htb", nameserver="10.129.
     else:
         return str(answer[0])
 
+def banner():
+
+	print("""	
+		___      __                               
+	____/ (_)____/ /___  ______  ____  ____  __  __
+	/ __  / / ___/ __/ / / / __ \/ __ \/ __ \/ / / /
+	/ /_/ / / /  / /_/ /_/ / /_/ / /_/ / / / / /_/ / 
+	\__,_/_/_/   \__/\__, / .___/\____/_/ /_/\__, /  
+					/____/_/                /____/   
+
+
+	0.1 - 2023-04-02 - Initial release
+
+	TODOs:
+
+	- Refine error handling
+	- Output file / logging
+
+	""")
+
 
 #
 # arguments
@@ -40,7 +60,7 @@ parser = argparse.ArgumentParser(
                         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 parser.add_argument("-d", "--domain", required=True, help="Specify the domain for which the subdomains will be enumerated")
 parser.add_argument("-n", "--nameserver", required=True, help="Specify the IP of the nameserver which should be used for enumeration")
-parser.add_argument("-q", "--querytype", required=True, help="Specify the type of query (A/TXT/NS)")
+parser.add_argument("-q", "--querytype", required=False, help="Specify the type of query (A/TXT/NS), queries for A/TXT/NS if not specified")
 parser.add_argument("-w", "--wordlist", required=True, type=argparse.FileType('r'), help="Specify the the wordlist you want to use")
 
 
@@ -48,23 +68,7 @@ parser.add_argument("-w", "--wordlist", required=True, type=argparse.FileType('r
 # script
 #         
 
-print("""	
-       ___      __                               
-  ____/ (_)____/ /___  ______  ____  ____  __  __
- / __  / / ___/ __/ / / / __ \/ __ \/ __ \/ / / /
-/ /_/ / / /  / /_/ /_/ / /_/ / /_/ / / / / /_/ / 
-\__,_/_/_/   \__/\__, / .___/\____/_/ /_/\__, /  
-                /____/_/                /____/   
-
-
-0.1 - 2023-04-02 - Initial release
-
-TODOs:
-
-- Refine error handling
-- Output file / logging
-
-""")
+banner()
 
 # check / load arguments
 args = parser.parse_args()
@@ -76,6 +80,12 @@ with open(args.wordlist.name) as wordlist:
 # go for it
 testdomain = "."+args.domain
 querytype = args.querytype
+
+# if args.querytype:
+#	querytype = args.querytype
+# else:
+#	querytype = ["A", "TXT", "NS"]
+
 nameserver = args.nameserver
 
 print("[-]"+"\t"+"Starting brute force subdomain enumeration with "+testdomain)
@@ -83,7 +93,7 @@ print("[-]"+"\t"+"Starting brute force subdomain enumeration with "+testdomain)
 for i in progressbar.progressbar(range(len(lines)), redirect_stdout=True):
 
 	testquery = lines[i]+testdomain
-	
+
 	try:
 		retval = dns_query_specific_nameserver(query=testquery,qtype=str(querytype),nameserver=nameserver)
 		print("[+]"+"\t"+testquery+" "+querytype+" "+retval)
